d57d135ba2418c28aa0bec618efa774cb726adf9
Pmalon1 <32411104+Pmalon1@users.noreply.github.com>
2018-01-16 23:45:03 +0100

    Update PROJEKT.cpp

--- PROJEKT/PROJEKT/PROJEKT.cpp
+++ PROJEKT/PROJEKT/PROJEKT.cpp
@@ -1,18 +1,18 @@
 
-#include "stdafx.h"					//Biblioteki programu
+#include "stdafx.h"
 #include <iostream>
 #include <conio.h>
 #include "windows.h"
 
 
 
-void wstep();						//Powiadamiam program o u¿ytych funkcjach 
+void wstep();
 void znak(char &z, int &);
 void rysowanie(char &z, int, int, int &);
 void poruszanie(int &, int &, int &);
 void koniec();
 
-using namespace std;				//U¿ycie przestrzeni nazw STD
+using namespace std;
 
 void gotoxy(int x, int y)
 {
@@ -48,18 +48,18 @@ void HideCursor()
 
 
 
-int main()						//Funkcja g³ówna programu
+int main()
 {
-	char z;						//Zmienna lokalna z
-	int x = 38, y = 11, bok;	//Wielkoœci zmiennych x i y odpowiadaj¹ce za punkt pocz¹tkowy programu oraz zmienna dlugosci boku
-	wstep();					//Wywolujaca sie funkcja wstep z informacjami na temat programu
-	znak(z, bok);				//Funkcja pobierajaca znak oraz dlugosc boku
-	do							//Pêtla wykonuj¹ca siê do naciœniêcia klawisza ESC
+	char z;
+	int x = 38, y = 11, bok;
+	wstep();
+	znak(z, bok);
+	do
 	{							
-		poruszanie(x, y, bok);	//Funkcja ruszania i powiekszania figury
-		rysowanie(z, x, y, bok);//Funkcja rysowania figury
+		poruszanie(x, y, bok);
+		rysowanie(z, x, y, bok);
 	} while (_getch() != 27);
-	koniec();					//Funkcja wyswietlajaca autora oraz mozliwoœæ powtórzenia programu
+	koniec();
 	return 0;
 }
 
@@ -67,7 +67,7 @@ int main()						//Funkcja g³ówna programu
 
 void wstep()
 {
-	system("cls");				//Czyszczenie ekranu na wypadek jego powtórzenia
+	system("cls");
 	cout << "Witaj w programie rysowania znaku!" << endl;
 	cout << "Program powinien umozliwic:" << endl;
 	cout << "   - wybor znaku kodu ASCII" << endl;
@@ -78,17 +78,17 @@ void wstep()
 	cout << endl << endl << endl;
 	cout << "\t\t\tNacisnij dowolny klawisz, aby przejsc dalej...";
 	_getch();
-	system("cls");				//Czyszczenie ekranu po wykonaniu funkcji
+	system("cls");
 
 }
 
 void znak(char &z,int &bok)
 {
 	cout << "Wybierz znak ASCII, ktory posluzy za budulec figury:";
-	cin >> z;					//Pobieranie znaku figury
+	cin >> z;
 	cout << "\nPodaj poczatkowy rozmiar figury (od 5 do 23):";
-	cin >> bok;					//Pobieranie wymiarów figury
-	while (bok < 5 || bok > 23)	//Pêtla sprawdzaj¹ca prawid³owe rozmiary figury
+	cin >> bok;
+	while (bok < 5 || bok > 23)
 	{
 		cout << "\nPodaj prawidlowy rozmiar figury!:"; cin >> bok;
 	} 
@@ -97,54 +97,54 @@ void znak(char &z,int &bok)
 void rysowanie(char &z, int x, int y, int &bok)
 {
 	
-	system("cls");											//Czyszczenie ekranu po wykonaniu funkcji znak
-	int x1 = x; int y1 = y;									//Inicjalizacja zmiennych pomocniczych
-	gotoxy(x, y);											//Ustawianie wspó³rzêdnych x i y
+	system("cls");
+	int x1 = x; int y1 = y;
+	gotoxy(x, y);
 
 
 
 
-	for (int i = x1 - (bok / 2); i <= x1+1; i++, y1++)		//Tworzenie lewego boku figury		
+	for (int i = x1 - (bok / 2); i <= x1+1; i++, y1++)	
 	{
 		gotoxy(i, y1 - bok / 2);
 		cout << z;
 	}
 
 
-	for (int i = x1; i <= x1 + bok / 2; i++, y1--)			//Tworzenie prawego	boku figury
+	for (int i = x1; i <= x1 + bok / 2; i++, y1--)
 	{
 		gotoxy(i-(bok/2), y1 + bok / 2 - (bok/2));
 		cout << z;
 	}
 
-	gotoxy(3, 24);											//Napis informuj¹cy na dole ekranu
+	gotoxy(3, 24);
 	cout <<"Nacisnij ESC, aby wyjsc...    " <<"Dlugosc boku:" << bok<<"    "<<"Wspolrzedne (x,y):"<<x<<"|"<<y;
 }
 
 
 void poruszanie(int &x, int &y, int &bok)
 {
-	HideCursor();					//Funkcja ukrywania kursora
-	char klawisz = _getch();		//Inicjalizacja klawisza u¿ytego w switchu
-	int x_l = (x - bok / 2);		//Zmienne pomocnicze
-	int x_p = (x);		//	
-	int y_g = (y - bok / 2);		//
-	int y_d = y;					//
+	HideCursor();
+	char klawisz = _getch();
+	int x_l = (x - bok / 2);
+	int x_p = (x);
+	int y_g = (y - bok / 2);
+	int y_d = y;
 
 
-	switch (klawisz)				//Switch s³u¿¹cy do poruszania i powiêkszania figury
+	switch (klawisz)
 	{
-	case 75:						//przesuniêcie w lewo
+	case 75:
 	{if(x_l>=1)x--; break; }
-	case 77:						//przesuniêcie w prawo
+	case 77:
 	{if (x_p<=76)x++; break; }
-	case 72:						//przesuniêcie do góry
+	case 72:
 	{if (y_g>=1)y--; break; }
-	case 80:						//przesuniêcie do do³u
+	case 80:
 	{if (y_d<=(20-(bok/2)))y++; break; }
-	case '+': {if ((x_l>=1)&&(x_p<=77)&&(y_g>=1)&& (y_d <= (20 - (bok / 2))))  bok += 2;	//Powiêkszanie obiektu
+	case '+': {if ((x_l>=1)&&(x_p<=77)&&(y_g>=1)&& (y_d <= (20 - (bok / 2))))  bok += 2;
 		break; }
-	case '-':{if (bok>=6) bok -= 2;						//Pomniejszanie obiektu 	
+	case '-':{if (bok>=6) bok -= 2;	
 		break; }								 	
 	}
 }	
@@ -152,16 +152,16 @@ void poruszanie(int &x, int &y, int &bok)
 
 void koniec()
 {
-	system("cls");				//Czyszczenie figury
-	int klawisz;				//Zmienna pomocnicza
-	gotoxy(0, 10);				//Napis na œrodku ekranu
+	system("cls");
+	int klawisz;
+	gotoxy(0, 10);
 	cout << "Jezeli chcesz powtorzyc program, nacisnij klawisz 'P'" << endl;
 	cout << "W innym przypadku, nacisnij dowolny klawisz..."<<endl;
-	gotoxy(0, 30);				//Napis na dole ekranu
+	gotoxy(0, 30);
 	cout << "Projekt wykonal: Piotr Malon" << endl;
 	cout << "Numer indeksu: s16721";
 	klawisz=_getch();
-	switch (klawisz)			//Switch s³u¿¹cy do powtórzenia funkcji g³ównej, czyli ca³ego programu
+	switch (klawisz)
 	{
 	case 'P': {main(); break; }
 	case 'p': {main(); break; }
